84a4bddc7fae79173053bbe201c9f8dcaea9f24a
Joerg Panzer <panzer@sub.uni-goettingen.de>
2014-07-08 09:04:35 +0200

    add package mongoDB

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,12 @@ Vagrant::Config.run do |config|
 
   config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
-  
-  config.vm.forward_port 27017, 27017 #mongodb
+  config.vm.forward_port 22, 2224, :auto => true
+  config.vm.forward_port 80, 4567
+  config.vm.forward_port 8080, 6789
+  config.vm.network :hostonly, "33.33.13.38" 
+ 
+#  config.vm.forward_port 27017, 27017 #mongodb
 
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "puppet/manifests"
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,18 +1,7 @@
-stage { pre: before => Stage[main] }
+group {'puppet': ensure => 'present'}
 
-class apt_get_update {
-  $sentinel = "/var/lib/apt/first-puppet-run"
-
-  exec { "initial apt-get update":
-    command => "/usr/bin/apt-get update && touch ${sentinel}",
-    onlyif  => "/usr/bin/env test \\! -f ${sentinel} || /usr/bin/env test \\! -z \"$(find /etc/apt -type f -cnewer ${sentinel})\"",
-    timeout => 3600,
-  }
-}
-
-# Run apt-get update prior to testing
-class { 'apt_get_update':
-  stage => pre,
+exec { "update-package-list":
+  command => "/usr/bin/sudo /usr/bin/apt-get update",
 }
 
 package { 'mongodb':
@@ -29,4 +18,50 @@ exec { 'allow remote mongo connections':
   notify  => Service['mongodb'],
   onlyif  => '/bin/grep -qx  "bind_ip = 127.0.0.1" /etc/mongodb.conf',
 }
+
+
+class java_7 {
+
+  package { "openjdk-7-jdk":
+    ensure => installed,
+    require => Exec["update-package-list"],
+  }
+
+}
+
+class tomcat_6 {
+  
+  package { "tomcat6":
+    ensure => installed,
+    require => Package['openjdk-7-jdk'],
+  }
+ 
+  package { "tomcat6-admin":
+    ensure => installed,
+    require => Package['tomcat6'],
+  }
+ 
+  service { "tomcat6":
+    ensure => running,
+    require => Package['tomcat6'],
+    subscribe => File["tomcat-users.xml"]
+  }
+ 
+  file { "tomcat-users.xml":
+    owner => 'root',
+    path => '/etc/tomcat6/tomcat-users.xml',
+    require => Package['tomcat6'],
+    notify => Service['tomcat6'],
+    content => template('/vagrant/templates/tomcat-users.xml.erb')
+  }
+ 
+}
+ 
+# set variables
+$tomcat_password = 'adminadmin'
+$tomcat_user = 'tomcat'
+ 
+
+include java_7
+include tomcat_6
 
